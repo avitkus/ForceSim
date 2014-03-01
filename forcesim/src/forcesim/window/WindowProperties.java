0a54aa38b2b1eee2856a42c7cae6e4221a6ecd74
daniel.chiquito@gmail.com <Daniel@Daniel-PC>
2014-03-01 16:37:11 -0500

    Removed width and height from WindowProperties

--- forcesim/src/forcesim/window/WindowProperties.java
+++ forcesim/src/forcesim/window/WindowProperties.java
@@ -7,7 +7,4 @@ public class WindowProperties {
 	public static int renderMode = RENDER_FALSE_COLOR;
 	
 	public static int scale = 40; // pixels per meter
-	
-	public static int width;
-	public static int height;
 }
