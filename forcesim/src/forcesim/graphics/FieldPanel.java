2fc15504d1308d7b18be3271107047cb9b9c570f
daniel.chiquito@gmail.com <Daniel@Daniel-PC>
2014-03-01 17:54:36 -0500

    Did fancy drawing shit

--- forcesim/src/forcesim/graphics/FieldPanel.java
+++ forcesim/src/forcesim/graphics/FieldPanel.java
@@ -5,27 +5,78 @@ import java.awt.Graphics;
 
 import javax.swing.JPanel;
 
+import forcesim.field.physics.ElectromagneticForce;
+import forcesim.field.physics.Field;
+import forcesim.field.physics.IField;
 import forcesim.field.physics.IPoint;
 import forcesim.field.physics.Point;
 import forcesim.field.physics.Vector2D;
 import forcesim.util.Util;
+import forcesim.window.WindowProperties;
 
 @SuppressWarnings("serial")
 public class FieldPanel extends JPanel {
 
 	private final Color COLOR_POSITIVE = Color.red;
 	private final Color COLOR_NEGATIVE = Color.blue;
+	
+	private final int RADIUS = 15;
 
+	private final IField field = new Field();
+
+	public FieldPanel() {
+		Point p = new Point(0,0);
+		p.setCharge(10);
+		field.addPoint(p);
+		p = new Point(1,-1);
+		p.setCharge(-10);
+		field.addPoint(p);
+	}
+	
 	@Override
 	public void paint(Graphics g) {
 		super.paint(g);
 
-		Point p = new Point(0,0);
-		p.setCharge(10);
-		renderPoint(g, p);
-		p = new Point(1,-1);
-		p.setCharge(-10);
-		renderPoint(g, p);
+		renderField(g);
+		
+		for (IPoint p: field.getPoints()) {
+			renderPoint(g, p);
+		}
+	}
+
+	private void renderField(Graphics g) {
+		double charge;
+		double maxCharge = 0;
+		
+		for (IPoint p : field.getPoints()) {
+			Vector2D v = Util.convertFieldCoordinate(this, p);
+			IPoint p2 = new Point(p.getX(), p.getY()+(((double)RADIUS-1)/WindowProperties.scale));
+			p2.setCharge(1);
+			double f = ElectromagneticForce.getVector(p, p2).getMagnitude();
+			if (f > maxCharge) maxCharge = f;
+		}
+		Point p;
+		
+		double[][] charges = new double[getWidth()][getHeight()];
+		for (int x=0; x<getWidth(); x++) {
+			for (int y=0; y<getHeight(); y++) {
+				p = Util.convertPixelCoordinate(this, x, y);
+				charge = field.getElectromagneticField(p.getX(), p.getY()).getMagnitude();
+				charges[x][y] = charge;
+			}
+		}
+		System.out.println("HAHAHAH "+maxCharge);
+		for (int x=0; x<getWidth(); x++) {
+			for (int y=0; y<getHeight(); y++) {
+				//System.out.println(charges[x][y]);
+				if (charges[x][y] < maxCharge) {
+					float cor = (float)(charges[x][y]/maxCharge);
+					//float cor = (float)Math.sqrt(Math.log10(9*(charges[x][y]/maxCharge)+1));
+					g.setColor(new Color(1f,1f-cor,1f-cor));
+					g.drawLine(x, y, x, y);
+				}
+			}
+		}
 	}
 
 	private void renderPoint(Graphics g, IPoint p) {
@@ -50,16 +101,16 @@ public class FieldPanel extends JPanel {
 		int halflength = 5;
 		g.setColor(Color.black);
 		g.drawLine((int)center.getX()-halflength, (int)center.getY(), (int)center.getX()+halflength, (int)center.getY());
-		
+
 	}
 
 	private void renderPointCircle(Graphics g, IPoint p, Color c) {
 		Vector2D center = Util.convertFieldCoordinate(this, p);
-		int radius = 15;
+		int radius = RADIUS;
 		g.setColor(Color.black);
 		g.fillOval((int)center.getX()-radius, (int)center.getY()-radius, 2*radius, 2*radius);
 		g.setColor(c);
-		radius = 13;
+		radius = radius-2;
 		g.fillOval((int)center.getX()-radius, (int)center.getY()-radius, 2*radius, 2*radius);
 	}
 }
