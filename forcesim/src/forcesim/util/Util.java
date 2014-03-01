ba57f4bc87a8a6955c0244bbd30993c336c101dd
daniel.chiquito@gmail.com <Daniel@Daniel-PC>
2014-03-01 16:23:44 -0500

    Update Util

--- forcesim/src/forcesim/util/Util.java
+++ forcesim/src/forcesim/util/Util.java
@@ -1,20 +1,21 @@
 package forcesim.util;
 
+import forcesim.field.physics.IPoint;
 import forcesim.field.physics.Point;
 import forcesim.field.physics.Vector2D;
 import forcesim.window.Window;
 
 public class Util {
-	public static Vector2D convertFieldCoordinate(Point p) {
+	public static Vector2D convertFieldCoordinate(IPoint p) {
 		return new Vector2D(
-				p.getX()*Window.getScale() + Window.getWidth(),
-				p.getY()*Window.getScale() + Window.getHeight()
+				p.getX()*Window.getScale() + Window.getWidth()/2,
+				p.getY()*Window.getScale() + Window.getHeight()/2
 				);
 	}
 	public static Point convertPixelCoordinate(int x, int y) {
 		return new Point(
-				(x-Window.getWidth())/(double)Window.getScale(),
-				(y-Window.getHeight())/(double)Window.getScale()
+				(x-Window.getWidth()/2)/(double)Window.getScale(),
+				(y-Window.getHeight()/2)/(double)Window.getScale()
 				);
 	}
 }
